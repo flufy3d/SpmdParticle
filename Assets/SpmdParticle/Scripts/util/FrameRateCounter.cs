b34184244a7457106319148f698d6fbcf3e6595e
flufy3d <flufy3d@gmail.com>
2018-05-03 12:43:38 +0200

    add camera control and fps display

---
+++ Assets/SpmdParticle/Scripts/util/FrameRateCounter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SpmdParticle;
+
+public class FrameRateCounter : MonoBehaviour
+{
+    public float m_update_interval = 0.5f;
+    public Text textFPS;
+    public Text textParticleNum;
+    private float m_last_time;
+    private float m_accum = 0.0f; // FPS accumulated over the interval
+    private int m_frames = 0; // Frames drawn over the interval
+    private float m_time_left; // Left time for current interval
+    private float m_result;
+
+    void Start()
+    {
+        m_time_left = m_update_interval;
+        m_last_time = Time.realtimeSinceStartup;
+    }
+
+    void Update()
+    {
+        float now = Time.realtimeSinceStartup;
+        float delta = now - m_last_time;
+        m_last_time = now;
+        m_time_left -= delta;
+        m_accum += 1.0f / delta;
+        ++m_frames;
+
+        // Interval ended - update result
+        if (m_time_left <= 0.0)
+        {
+            m_result = m_accum / m_frames;
+            textFPS.text = "FPS: " + m_result.ToString("f2");
+            textParticleNum.text = "Particles: " + SPWorld.s_instances[0].m_particle_num;
+            m_time_left = m_update_interval;
+            m_accum = 0.0f;
+            m_frames = 0;
+        }
+    }
+}
